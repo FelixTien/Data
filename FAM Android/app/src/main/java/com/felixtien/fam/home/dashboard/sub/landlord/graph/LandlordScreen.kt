495a2a41594187b13c6ea7f409622402a88bb946
Felix Tien <felix95071919@gmail.com>
2024-08-30 08:29:40 -0400

    Felix Tien

--- FAM Android/app/src/main/java/com/felixtien/fam/home/dashboard/sub/landlord/graph/LandlordScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.felixtien.fam.home.dashboard.sub.landlord.graph
-
-sealed class LandlordScreen(val route: String) {
-    data object Landlord: LandlordScreen(route = "landlord_screen")
-    data object LeaseList: LandlordScreen(route = "lease_list_screen")
-    data object Lease: LandlordScreen(route = "lease_screen")
-    data object EditLease: LandlordScreen(route = "edit_lease_screen")
-}
--- FAM Android/app/src/main/java/com/felixtien/fam/home/management/graph/ManagementScreen.kt
+++
@@ -1,6 +0,0 @@
-package com.felixtien.fam.home.management.graph
-
-sealed class ManagementScreen(val route: String) {
-    data object Management: ManagementScreen(route = "management_screen")
-    data object Click: ManagementScreen(route = "click_screen")
-}
